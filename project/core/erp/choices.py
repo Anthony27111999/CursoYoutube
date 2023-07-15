1db6a2de56e441f296823742d8883518eaea3b5a
Anthony27111999 <sosa27111999@gmail.com>
2023-07-15 05:06:52 -0300

    Agrego tablas

---
+++ project/core/erp/choices.py
@@ -0,0 +1,4 @@
+gender_choice =(
+    ('man', 'male'),
+    ('woman', 'famele')
+)
--- project/core/erp/models.py
+++ project/core/erp/models.py
@@ -1,53 +1,80 @@
 from django.db import models
 from datetime import datetime
-
-
-class Item(models.Model):
-    name = models.CharField(max_length=150, verbose_name='Nombre', default="")
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name = 'Tipo'
-        verbose_name_plural = 'Tipos'
-        db_table = 'tipo'
-        ordering = ['id']
+from core.erp.choices import gender_choice
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=150, verbose_name='Nombre', default="")
-
-
-class Employee(models.Model):
-    categ = models.ManyToManyField(Category)
-    type = models.ForeignKey(Item, on_delete=models.CASCADE)
-    name = models.CharField(max_length=150, verbose_name='Nombre', default="")
-    ci = models.CharField(max_length=8, verbose_name='C.I')
-    date_create = models.DateTimeField(auto_now=True)
-    date_update = models.DateTimeField(auto_now_add=True)
-    age = models.PositiveIntegerField(default=0)
-    salary = models.DecimalField(default=00.00, max_digits=2, decimal_places=2)
-    state = models.BooleanField(default=True)
-    geder = models.CharField(max_length=50)
-    avatar = models.ImageField(upload_to='avatar')
-    CVitae = models.FileField()
+    name = models.CharField(max_length=150, verbose_name='Nombre', unique=True)
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = 'Empleado'
-        verbose_name_plural = 'Empleados'
-        db_table = 'empleado'
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
         ordering = ['id']
 
 
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Nombre', unique=True)
+    cate = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='product/%Y/%m/%d', null=True, blank=True)
+    pvp = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Producto'
+        verbose_name_plural = 'Productos'
+        ordering = ['id']
+
+class Client(models.Model):
+    names = models.CharField(max_length=150, verbose_name='Nombres')
+    surnames = models.CharField(max_length=150, verbose_name='Apellidos')
+    ci = models.CharField(max_length=8, verbose_name='C.I')
+    birthday = models.DateField(default=datetime.now, verbose_name='Decha de nacimiento')
+    address = models.CharField(max_length=150,null=True,verbose_name='Direccion', blank=True)
+    sexo = models.CharField(max_length=10, choices=gender_choice, default='male', verbose_name='Sexo')
+
+    def __str__(self):
+        return self.names
+
+    class Meta:
+        verbose_name = 'Cliente'
+        verbose_name_plural = 'CLientes'
+        ordering = ['id']
 
 
+class Sale(models.Model):
+    cli = models.ForeignKey(Client, on_delete=models.CASCADE)
+    date_joined = models.DateField(default=datetime.now())
+    subtotal = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
+    iva = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
+    total = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
 
+    def __str__(self):
+        return str(self.id)
 
+    class Meta:
+        verbose_name = 'Venta'
+        verbose_name_plural = 'Ventas'
+        ordering = ['id']
 
+class DetSale(models.Model):
+    sale = models.ForeignKey(Sale, on_delete=models.CASCADE)
+    prod = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
+    cant = models.IntegerField(default=0)
+    subtotal = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
+
+    def __str__(self):
+        return self.prod.name
+
+    class Meta:
+        verbose_name = 'DetalleVenta'
+        verbose_name_plural = 'DetalleVentas'
+        ordering = ['id']
 
 
 
